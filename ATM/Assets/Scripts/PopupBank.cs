93ecb42d5e523bee1c8245018ecda9422e1cd09d
jkh201020 <jack0517@naver.com>
2025-06-04 19:38:09 +0900

    feat: 입금, 출금 버튼UI와 스크립트 연결

--- ATM/Assets/Scripts/PopupBank.cs
+++ ATM/Assets/Scripts/PopupBank.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class PopupBank : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject deposit;
+    [SerializeField] private GameObject withdrawal;
+   
+    public void OnDepositButtonClick()
+    {
+        deposit.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void OnWithdrawalButtonClick()
+    {
+        withdrawal.SetActive(true);
+        gameObject.SetActive(false);
+    }
 }
