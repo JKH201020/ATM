328e06a063deb793fc19c6b169809e895154d8c1
jkh201020 <jack0517@naver.com>
2025-06-05 12:08:25 +0900

    feat: 저장 불러오기 기능 구현
    유저 데이터 값이 변하면 UI가 변하는 동시에 그 값을 저장하고,
    게임을 껐다가 켜면 전에 저장한 데이터를 불러온다.

--- ATM/Assets/Scripts/GameManager.cs
+++ ATM/Assets/Scripts/GameManager.cs
@@ -20,14 +20,17 @@ public class GameManager : MonoBehaviour
     void Awake() // 초기화
     {
         Instance = this;
-        
-        if (userData == null)
-        {
-            // 유저 정보 초기화
-            userData = new UserData("", 0, 0);
-        }
 
-        Refresh();
+        // LoadUserData()에서 저장된 값이 없으면 초기값을 할당해주기 때문에
+        // 이 조건문은 필요가 없어짐
+        // if (userData == null)
+        // {
+        //     // 유저 정보 초기화
+        //     userData = new UserData("", 0, 0);
+        // }
+
+        LoadUserData(); // 저장된 데이터 불러옴
+        Refresh(); // UI 업데이트
     }
 
     public void DepositCash(int amount) // 입금 - 버튼에 연결
@@ -65,5 +68,32 @@ public class GameManager : MonoBehaviour
         nameText.text = userData.Name;
         cashText.text = string.Format("{0:N0}", userData.Cash);
         balanceText.text = string.Format("Balance   {0:N0}", userData.Balance);
+        SaveUserData(); // UI가 업데이트 되면 업데이트된 데이터 값을 저장
     }
+
+    #region 데이터 세이브, 로드
+
+    public void SaveUserData() // 유저 데이터 저장
+    {
+        // 첫 번째 인자는 "Key"라는 저장 공간이고 "Key"의 이름에 따라 저장된 값을 불러온다.
+        // 두 번째 인자는 저장할 데이터를 입력한다.
+        PlayerPrefs.SetString("Name", userData.Name);
+        PlayerPrefs.SetInt("Balance", userData.Balance);
+        PlayerPrefs.SetInt("Cash", userData.Cash);
+
+        PlayerPrefs.Save(); // 위의 내용들을 저장
+    }
+
+    public void LoadUserData() // 유저 데이터 불러오기
+    {
+        // "Key"의 이름에 맞는 저장된 값을 불러온다.
+        // 저장된 값이 없을 경우 두 번째 인자를 기본값으로 불러온다.
+        userData.Name = PlayerPrefs.GetString("Name", "");
+        userData.Balance = PlayerPrefs.GetInt("Balance", 0);
+        userData.Cash = PlayerPrefs.GetInt("Cash", 0);
+
+        Refresh();
+    }
+
+    #endregion
 }
